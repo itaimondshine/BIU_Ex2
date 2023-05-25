e6f08e6b679cc02504033c8edf51289fad1e9606
itaimondshine <52546751+itaimondshine@users.noreply.github.com>
2023-05-25 11:16:46 +0300

    more refactors

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -86,18 +86,6 @@ def generate_ngrams(word, n):
 
 
 def calculate_ngram_fitness(ngrams, ngram_frequency, ngram_weight):
-    """
-    This function calculates the fitness of a text based on the frequency of ngrams.
-
-    Args:
-      ngrams: The ngrams in the text.
-      ngram_frequency: The frequency of each ngram.
-      ngram_weight: The weight of each ngram.
-
-    Returns:
-      The fitness of the text.
-    """
-
     return sum([ngram_frequency[ngram] * ngram_weight for ngram in ngrams if
                 ngram in ngram_frequency and ngram_weight > 0])
 
@@ -109,7 +97,24 @@ def mutate_key(key):
     return ''.join(key)
 
 
-class GeneticSolver:
+def get_char_trigram_dict(file_name):
+
+    with open(file_name, 'r') as f:
+        words = f.read().replace('\n', '')
+
+    char_trigram_dict = {}
+    for i in range(len(words) - 2):
+        char_trigram = words[i] + words[i + 1] + words[i + 2]
+        if char_trigram.isalpha():
+            if char_trigram not in char_trigram_dict:
+                char_trigram_dict[char_trigram.upper()] = 1
+            else:
+                char_trigram_dict[char_trigram.upper()] += 1
+
+    return char_trigram_dict
+
+
+class GeneticSubstitutionSolver:
     def __init__(self, ciphertext):
         self.ciphertext = ciphertext
         self.lettercase = [ch.islower() and ch.isalpha() for ch in self.ciphertext]
@@ -118,7 +123,7 @@ class GeneticSolver:
         self.unigram_frequency = read_letter_frequencies(UNIGRAM_FILENAME_PATH)
         self.set_of_words = set(get_list_of_words(DICT_FILENAME_PATH))
         self.set_of_words_upper = {word.upper() for word in self.set_of_words}
-        self.trigram_frequency = self.get_char_trigram_dict(DICT_FILENAME_PATH)
+        self.trigram_frequency = get_char_trigram_dict(DICT_FILENAME_PATH)
         self.generations = 500
         self.population_size = 500
         self.tournament_size = 20
@@ -148,30 +153,12 @@ class GeneticSolver:
             probability = self.tournament_probabilities[i - 1] * (1.0 - self.tournament_winner_probability)
             self.tournament_probabilities.append(probability)
 
-    def get_char_trigram_dict(self, file_name):
-        """
-        Get a dict of mapping between char trigram and number of appreances in the text.
-
-        Args:
-          file_name: The name of the file to read.
-
-        Returns:
-          A dict of mapping between char trigram and number of appreances in the text.
-        """
-
-        with open(file_name, 'r') as f:
-            words = f.read().replace('\n', '')
-
-        char_trigram_dict = {}
-        for i in range(len(words) - 2):
-            char_trigram = words[i] + words[i + 1] + words[i + 2]
-            if char_trigram.isalpha():
-                if char_trigram not in char_trigram_dict:
-                    char_trigram_dict[char_trigram.upper()] = 1
-                else:
-                    char_trigram_dict[char_trigram.upper()] += 1
-
-        return char_trigram_dict
+    def info_display(self, decrypted_text, highest_fitness, key, no):
+        plaintext = self.convert_to_plaintext(decrypted_text)
+        print('[Generation ' + str(no) + ']', )
+        print('Max Fitness:', highest_fitness)
+        print('Key:', key)
+        print('Decrypted Text:\n' + plaintext + '\n')
 
     def decrypt(self, key):
         letter_mapping = {self.letters[i]: key.upper()[i] for i in range(26)}
@@ -183,15 +170,6 @@ class GeneticSolver:
         return decrypted_text
 
     def calculate_key_fitness(self, text):
-        """
-        This function calculates the fitness of a text based on its unigrams, bigrams, and trigrams.
-
-        Args:
-          text: The text to calculate the fitness of.
-
-        Returns:
-          The fitness of the text.
-        """
 
         unigrams = generate_ngrams(text, 1) if self.unigram_weight else []
         bigrams = generate_ngrams(text, 2)
@@ -255,34 +233,22 @@ class GeneticSolver:
         offspring_two = self.combine_strings(parent_two, parent_one)
         return offspring_one, offspring_two
 
-    def reproduction(self, population, fitness):
-        """
-        This function reproduces the population based on the fitness of each individual.
-
-        Args:
-          population: The current population.
-          fitness: The fitness of each individual in the population.
-
-        Returns:
-          The new population.
-        """
+    def reproduction(self, pop, fit):
 
         crossover_population = []
 
         while len(crossover_population) < self.crossover_count:
 
             if self.selection_method == 'RWS':
-                parent_one_index = self.select_by_tournament(population, fitness)
-                parent_two_index = self.select_by_roulette(fitness)
+                parent_one_index = self.select_by_tournament(pop, fit)
+                parent_two_index = self.select_by_roulette(fit)
 
-                parent_one = population[parent_one_index]
-                parent_two = population[parent_two_index]
+                parent_one = pop[parent_one_index]
+                parent_two = pop[parent_two_index]
             else:
-                parent_one, parent_two = self.select_by_tournament(population, fitness)
+                parent_one, parent_two = self.select_by_tournament(pop, fit)
 
-            offspring_one, offspring_two = self.generate_offspring(parent_one, parent_two)
-
-            crossover_population += [offspring_one, offspring_two]
+            crossover_population += [*self.generate_offspring(parent_one, parent_two)]
 
         crossover_population = self.mutation(crossover_population, self.crossover_count)
 
@@ -337,14 +303,13 @@ class GeneticSolver:
         return population
 
     def solve(self, solver=SolverType.REGULAR):
-        # Main Program
         population = self.initialization()
 
         key = ''
         plaintext = ''
         highest_fitness = 0
-        stuck_counter = 0
-        for no in range(self.generations + 1):
+        convergence_counter = 0
+        for gen in range(self.generations + 1):
 
             population, fitness = self.new_gen(population)
             if solver != SolverType.REGULAR:
@@ -355,29 +320,20 @@ class GeneticSolver:
                 population = optimized_population
             # Terminate if highest_fitness not increasing
             if highest_fitness == max(fitness):
-                stuck_counter += 1
+                convergence_counter += 1
             else:
-                stuck_counter = 0
+                convergence_counter = 0
 
-            if stuck_counter >= self.terminate:
+            if convergence_counter >= self.terminate:
                 break
 
             highest_fitness, key = get_best_results(population, fitness)
 
             plaintext = self.convert_to_plaintext(self.decrypt(key))
 
-            # if self.verbose:
-            #     self.verbose_display(sum(fitness) / self.population_size, plaintext, highest_fitness, key, no)
+            self.info_display(plaintext, highest_fitness, key, gen)
 
         save_plain_to_file(plain_text=plaintext)
         save_perm_to_file(key=key)
 
         return plaintext
-
-    def verbose_display(self, average_fitness, decrypted_text, highest_fitness, key, no):
-        plaintext = self.convert_to_plaintext(decrypted_text)
-        print('[Generation ' + str(no) + ']', )
-        print('Average Fitness:', average_fitness)
-        print('Max Fitness:', highest_fitness)
-        print('Key:', key)
-        print('Decrypted Text:\n' + plaintext + '\n')
