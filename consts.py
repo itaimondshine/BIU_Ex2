40d9dbfa37e68463f9d9add9342453108827c205
itaimondshine <52546751+itaimondshine@users.noreply.github.com>
2023-05-31 20:37:43 +0300

    remove output

--- consts.py
+++ consts.py
@@ -3,10 +3,9 @@ import string
 BIGRAMS_FILENAME_PATH = 'Letter2_Freq.txt'
 UNIGRAMS_FILENAME_PATH = 'Letter_Freq.txt'
 DICT_FILENAME_PATH = 'dict.txt'
-PERM_FILE_PATH = 'output/perm.txt'
-PLAIN_FILE_PATH = 'output/plain.txt'
+PERM_FILE_PATH = 'perm.txt'
+PLAIN_FILE_PATH = 'plain.txt'
 LETTERS = set(string.ascii_lowercase)
 UPPER_LETTERS = set(string.ascii_uppercase)
-
 
 
